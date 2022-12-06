3afea7102cd8ee8cf8c2535585db985f356b66f7
BunQ707 <adung2912d03@gmail.com>
2022-12-06 11:22:25 +0700

    impl new defuzz logic

--- src/modules/nutrition/nutrition.service.ts
+++ src/modules/nutrition/nutrition.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import Decimal from 'decimal.js';
 import { calculateBMI } from 'src/shared/helper';
 import { NutritionDto } from './nutrition.dto';
+import { Name } from './common';
+import { think } from './rules';
 
-import Rules from './rules';
+import Plans from './plans';
 
 const Logic = require('es6-fuzz/lib/logic');
 const Trapezoid = require('es6-fuzz/lib/curve/trapezoid');
@@ -10,24 +13,16 @@ const Grade = require('es6-fuzz/lib/curve/grade');
 const ReverseGrade = require('es6-fuzz/lib/curve/reverse-grade');
 const Triangle = require('es6-fuzz/lib/curve/triangle');
 
-enum Name {
-  Male = 'Male',
-  Female = 'Female',
-  Type1 = 'TypeOne',
-  Type2 = 'TypeTwo',
-  Risk = 'Risk',
-  Children = 'Children',
-  Adolescence = 'Adolescence',
-  Adult = 'Adult',
-  Older = 'Older',
-  Obese = 'Obese',
-  Sedentary = 'Sedentary',
-  Moderate = 'Moderate',
-  Active = 'Active',
-  UnderWeight = 'UnderWeight',
-  Normal = 'Normal',
-  OverWeight = 'OverWeight',
-}
+type FRule = {
+  output: Name;
+  fuzzy: number;
+};
+
+type FRes = {
+  fuzzified: number;
+  defuzzified: Name;
+  rules: FRule[];
+};
 
 @Injectable({})
 export class NutritionService {
@@ -64,38 +59,120 @@ export class NutritionService {
     const diabetesTypeRes = diabetesTypeLogic.defuzzify(
       input.DiabetesType || 0,
     );
-    const ageGroupRes = ageGroupLogic.defuzzify(input.Age || 0);
+    const ageGroupRes: FRes = ageGroupLogic.defuzzify(input.Age || 0);
     // const activityFactorRes = activityFactorLogic.defuzzify(
     //   input.ActivityFactor || 0,
     // );
-    const bmiRes = bmiLogic.defuzzify(BMI || 0);
+    const bmiRes: FRes = bmiLogic.defuzzify(BMI || 0);
     if (bmiRes) {
     }
 
+    // let recommendList = [];
+
+    // if (diabetesTypeRes?.rules[0]?.fuzzy > 0) recommendList = [...Rules.type0];
+    // else if (
+    //   diabetesTypeRes?.rules[1]?.fuzzy > 0 ||
+    //   diabetesTypeRes?.rules[2]?.fuzzy > 0
+    // ) {
+    //   // general
+    //   recommendList = [...recommendList, ...Rules.type12.general];
+
+    //   //  type 1 and 2
+    //   if (diabetesTypeRes?.rules[1]?.fuzzy > 0)
+    //     recommendList = [...recommendList, ...Rules.type12.type1];
+    //   if (diabetesTypeRes?.rules[2]?.fuzzy > 0)
+    //     recommendList = [...recommendList, ...Rules.type12.type2];
+
+    //   // older
+    //   if (ageGroupRes?.rules[3]?.fuzzy > 0)
+    //     recommendList = [...recommendList, ...Rules.type12.older];
+
+    //   // female
+    //   if (genderRes?.rules[1]?.fuzzy > 0)
+    //     recommendList = [...recommendList, ...Rules.type12.female];
+    // }
+
+    // return { rules: recommendList };
+
+    // dò các luật mờ cần duyệt và giá trị mờ cho luật đó
+    let fusedArray = [];
+    for (let ageRule of ageGroupRes.rules) {
+      if (new Decimal(ageRule.fuzzy).greaterThan(0)) {
+        for (let bmiRule of bmiRes.rules) {
+          if (new Decimal(bmiRule.fuzzy).greaterThan(0)) {
+            fusedArray.push({
+              bmiSet: bmiRule.output,
+              ageSet: ageRule.output,
+              minFuzzy: Decimal.min(
+                new Decimal(ageRule.fuzzy),
+                new Decimal(bmiRule.fuzzy),
+              ),
+            });
+          }
+        }
+      }
+    }
+
+    let plan1Arr = [];
+    let plan2Arr = [];
+    let plan3Arr = [];
+
+    // Duyệt các luật mờ để lấy giá trị Plan, gán với giá trị mờ trước đó
+    fusedArray.forEach(
+      (fa: { bmiSet: Name; ageSet: Name; minFuzzy: Decimal }) => {
+        const thinkRes = think(
+          fa.ageSet,
+          fa.bmiSet,
+          diabetesTypeRes.defuzzified,
+          genderRes.defuzzified,
+        );
+        if (thinkRes === 1) plan1Arr.push(fa.minFuzzy);
+        if (thinkRes === 2) plan2Arr.push(fa.minFuzzy);
+        if (thinkRes === 3) plan3Arr.push(fa.minFuzzy);
+      },
+    );
+
+    // Duyệt lại các Plan, lấy giá trị mờ lớn nhất
+    let plan1Value: Decimal =
+      plan1Arr.length > 0
+        ? plan1Arr.reduce((a, b) => {
+            return Decimal.max(a, b);
+          })
+        : new Decimal(0);
+    let plan2Value: Decimal =
+      plan2Arr.length > 0
+        ? plan2Arr.reduce((a, b) => {
+            return Decimal.max(a, b);
+          })
+        : new Decimal(0);
+    let plan3Value: Decimal =
+      plan3Arr.length > 0
+        ? plan3Arr.reduce((a, b) => {
+            return Decimal.max(a, b);
+          })
+        : new Decimal(0);
+
+    // Khử mờ
+    const result = new Decimal(
+      plan1Value
+        .plus(plan2Value.mul(2))
+        .plus(plan3Value.mul(3))
+        .div(plan1Value.plus(plan2Value).plus(plan3Value)),
+    );
+
     let recommendList = [];
 
-    if (diabetesTypeRes?.rules[0]?.fuzzy > 0) recommendList = [...Rules.type0];
-    else if (
-      diabetesTypeRes?.rules[1]?.fuzzy > 0 ||
-      diabetesTypeRes?.rules[2]?.fuzzy > 0
-    ) {
-      // general
-      recommendList = [...recommendList, ...Rules.type12.general];
-
-      //  type 1 and 2
-      if (diabetesTypeRes?.rules[1]?.fuzzy > 0)
-        recommendList = [...recommendList, ...Rules.type12.type1];
-      if (diabetesTypeRes?.rules[2]?.fuzzy > 0)
-        recommendList = [...recommendList, ...Rules.type12.type2];
-
-      // older
-      if (ageGroupRes?.rules[3]?.fuzzy > 0)
-        recommendList = [...recommendList, ...Rules.type12.older];
-
-      // female
-      if (genderRes?.rules[1]?.fuzzy > 0)
-        recommendList = [...recommendList, ...Rules.type12.female];
-    }
+    if (result.greaterThan(0) && result.lessThanOrEqualTo(1))
+      recommendList = Plans.Plan1;
+    else if (result.greaterThan(1) && result.lessThan(2))
+      recommendList = [...Plans.Plan1, ...Plans.Plan2];
+    else if (result.equals(2)) recommendList = Plans.Plan2;
+    else if (result.greaterThan(2) && result.lessThan(3))
+      recommendList = [...Plans.Plan2, ...Plans.Plan3];
+    else if (result.equals(3)) recommendList = Plans.Plan3;
+    else if (result.greaterThan(3) && result.lessThan(4))
+      recommendList = Plans.Plan3;
+    else recommendList = [];
 
     return { rules: recommendList };
   }
