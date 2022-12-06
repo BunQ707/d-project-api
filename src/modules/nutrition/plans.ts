01ee4352c8048eff904890f56d0929479de1ccc0
BunQ707 <adung2912d03@gmail.com>
2022-12-06 11:20:12 +0700

    add new rule, new plan

---
+++ src/modules/nutrition/plans.ts
@@ -0,0 +1,63 @@
+const Rules = {
+  type0: [
+    'No nutrition recommendation can be made for preventing type 1 diabetes',
+    'Among individuals at high risk for developing type 2 diabetes: reduced intake of fat; achieve the USDA recommendation for dietary fiber and foods containing whole grains; low– glycemic index foods: Green vegetables, most fruits, raw carrots, kidney beans, chickpeas, lentils and bran breakfast cereals, etc are encouraged.',
+  ],
+  type12: {
+    general: [
+      'A dietary pattern that includes carbohydrate from fruits, vegetables, whole grains, legumes, and low-fat milk is encouraged for good health.',
+      'Low-carbohydrate diets, restricting total carbohydrate to 130 g/day, are not recommended in the management of diabetes.',
+      'Care should be taken to avoid excess energy intake.',
+      'Sucrose-containing foods can be substituted for other carbohydrates in the meal plan.',
+      'Fiber-containing foods',
+      'Sugar alcohols and nonnutritive sweeteners are safe when consumed within the daily intake levels established by the FDA.',
+      'Limit saturated fat to 7% of total calories.',
+      'The absorption of trans fats should be minimized. Examples: cookies and pies, microwave popcorn, frozen pizza, cookies and rolls, donuts, and more.',
+      'Two or more servings of fish per week are recommended.',
+      'High-protein diets are not recommended.',
+      'Vitamins E and C and carotene, is not advised.',
+      'Routine supplementation of vitamins E and C and carotene is not advised.',
+    ],
+    type1: [
+      'Insulin therapy should be integrated into an individual’s dietary and physical activity pattern',
+    ],
+    type2: ['Physical activity'],
+    older: [
+      'Modest energy restriction and an increase in physical activity.',
+      'A daily multivitamin supplement may be appropriate, especially for those older adults with reduced energy intake.',
+    ],
+    female: [
+      'Nutrition interventions for pregnancy and lactation with diabetes: Adequate energy intake that provides appropriate weight gain is recommended during pregnancy. Weight loss is not recommended; however, for overweight and obese women with GDM, modest energy and carbohydrate restriction may be appropriate.',
+    ],
+  },
+};
+
+const Plans = {
+  Plan1: [
+    'No nutrition recommendation can be made for preventing type 1 diabetes',
+    'Among individuals at high risk for developing type 2 diabetes: reduced intake of fat; achieve the USDA recommendation for dietary fiber and foods containing whole grains; low– glycemic index foods: Green vegetables, most fruits, raw carrots, kidney beans, chickpeas, lentils and bran breakfast cereals, etc are encouraged.',
+  ],
+  Plan2: [
+    'A dietary pattern that includes carbohydrate from fruits, vegetables, whole grains, legumes, and low-fat milk is encouraged for good health.',
+    'Low-carbohydrate diets, restricting total carbohydrate to 130 g/day, are not recommended in the management of diabetes.',
+    'Care should be taken to avoid excess energy intake.',
+    'Sucrose-containing foods can be substituted for other carbohydrates in the meal plan.',
+    'Fiber-containing foods',
+    'Sugar alcohols and nonnutritive sweeteners are safe when consumed within the daily intake levels established by the FDA.',
+    'Physical activity',
+  ],
+  Plan3: [
+    'Limit saturated fat to 7% of total calories.',
+    'The absorption of trans fats should be minimized. Examples: cookies and pies, microwave popcorn, frozen pizza, cookies and rolls, donuts, and more.',
+    'Two or more servings of fish per week are recommended.',
+    'High-protein diets are not recommended.',
+    'Vitamins E and C and carotene, is not advised.',
+    'Routine supplementation of vitamins E and C and carotene is not advised.',
+    'Insulin therapy should be integrated into an individual’s dietary and physical activity pattern',
+    'Physical activity',
+    'Modest energy restriction and an increase in physical activity.',
+    'A daily multivitamin supplement may be appropriate, especially for those older adults with reduced energy intake.',
+  ],
+};
+
+export default Plans;
